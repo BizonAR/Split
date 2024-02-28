8c3de44493909bbf807876dd5a52c3fc478a8bc4
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 12:20:27 +0500

    Split

---
+++ Split.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+	internal class Split
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите строку с текстом: ");
+
+			string stringText = Console.ReadLine();
+
+			char spaceCharacter = ' ';
+			string[] subStrings = stringText.Split(spaceCharacter);
+
+			for (int i = 0; i < subStrings.Length; i++)
+			{
+				Console.WriteLine(subStrings[i]);
+			}
+		}
+	}
+}
